4d573148c5172db3483de069ce4cc384ffee3c67
Jack Sullivan <me@jackhsullivan.com>
2023-03-11 18:03:58 -0800

    yeet skeet reepeat

--- run_agent.py
+++ run_agent.py
@@ -4,6 +4,13 @@ import pickle
 from minerl.herobraine.env_specs.human_survival_specs import HumanSurvival
 
 from agent import MineRLAgent, ENV_KWARGS
+import logging
+
+import coloredlogs
+
+coloredlogs.install(logging.DEBUG)
+# logging.basicConfig(level=logging.DEBUG)
+
 
 def main(model, weights):
     env = HumanSurvival(**ENV_KWARGS).make()
@@ -12,7 +19,8 @@ def main(model, weights):
     policy_kwargs = agent_parameters["model"]["args"]["net"]["args"]
     pi_head_kwargs = agent_parameters["model"]["args"]["pi_head_opts"]
     pi_head_kwargs["temperature"] = float(pi_head_kwargs["temperature"])
-    agent = MineRLAgent(env, policy_kwargs=policy_kwargs, pi_head_kwargs=pi_head_kwargs)
+    agent = MineRLAgent(env, policy_kwargs=policy_kwargs,
+                        pi_head_kwargs=pi_head_kwargs)
     agent.load_weights(weights)
 
     print("---Launching MineRL enviroment (be patient)---")
@@ -27,8 +35,10 @@ def main(model, weights):
 if __name__ == "__main__":
     parser = ArgumentParser("Run pretrained models on MineRL environment")
 
-    parser.add_argument("--weights", type=str, required=True, help="Path to the '.weights' file to be loaded.")
-    parser.add_argument("--model", type=str, required=True, help="Path to the '.model' file to be loaded.")
+    parser.add_argument("--weights", type=str, required=True,
+                        help="Path to the '.weights' file to be loaded.")
+    parser.add_argument("--model", type=str, required=True,
+                        help="Path to the '.model' file to be loaded.")
 
     args = parser.parse_args()
 
